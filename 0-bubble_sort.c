270db46a8e6ca15632b44aa0b03e7d8a83790d86
Adepoju Usman <adepojuusman465@gmail.com>
2023-01-24 11:38:05 +0000

    First commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "sort.h"
+
+
+/**
+* bubble_sort - sort array using bubble sort algorithm
+* @array: array to sort
+* @size: size(length) of the array
+*
+* Return: NULL
+*/
+
+void bubble_sort(int *array, size_t size);
+{
+	int i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+				swap(array[j], array[j + 1])
+				print_array(array, size)
+		}
+	}
+}
---
+++ sort.h
@@ -0,0 +1,57 @@
+#ifndef SORT_H
+#define SORT_H
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+
+
+//functions
+void swap(int *n1, int *n2)
+{
+    int *tmp = *n1;
+    *n1 = *n2;
+    *n2 = *tmp
+}
+
+
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
+
+//prototypes
+void bubble_sort(int *array, size_t size);
+
+
+
+#endif
